0f6b1164c9455b52f46a5414e7ab671472050011
govindrajputFST <153702606+govindrajputFST@users.noreply.github.com>
2024-04-09 15:25:18 +0530

    few Changes

--- src/controllers/libraryController.mjs
+++ src/controllers/libraryController.mjs
@@ -1,11 +1,53 @@
 import libraryModel from "../models/libraryModel.mjs";
-const findBook=async (req,res)=>{
- const books=await libraryModel.find();
- return res.send({status:"OK",message:books});
+const findBook = async (req, res) => {
+    try {
+        const books = await libraryModel.find();
+     return res.status(200).send({ status: "Status 200 Array of books matching the search criteria", message: books });
+    } catch (error) {
+        return res.status(400).send({ status: 'Status 400 invalid search parameter', message: error.message });
+    }
 }
-const createBook=async (req,res)=>{
-    const  data=req.body;
-    const addBooks= await libraryModel.create(data);
-    return res.send({status:'ok',message:addBooks});
+
+const createBook = async (req, res) => {
+    try {
+        const data = req.body;
+    const addBooks = await libraryModel.create(data);
+     return res.status(201).send({ Success: "Status 201 Book added successful",message:addBooks })
+    
+    } catch (error) {
+        return res.status(400).send({ Error: "Status 400 Invalid request data" })
+     
+    }
+    
+    
 }
-export {findBook,createBook}
+const updateBook = async (req, res) => {
+    try {
+        const  {id } = req.params;
+        const updatedBook = await libraryModel.findByIdAndUpdate({_id:id}, {$set :{"title": "newBook" }});
+        if (!updatedBook) {
+            return res.status(404).send({ Error: "Status 404 Book not found" })
+        }
+        return res.status(200).send({ Success: " Status 200 Book updated successfully", data: updatedBook });
+    } catch (error) {
+        return res.status(500).send({ status: 'failed', message: error.message });
+    }
+}
+const deleteBook = async (req, res) => {
+    try {
+        const {id}=req.params;
+        console.log(id)
+        const data = await libraryModel.findByIdAndDelete({_id:id });
+        if (!data) {
+            return res.status(404).send({ Error: "Status 404 Book not found" })
+            }
+    return res.status(200).send({ Success: "Status 200 Book updated successfully", message: data })
+     
+    } catch (error) {
+        return res.status(500).send({status:'falied',message: error.message})
+    }
+}
+  
+
+export { findBook, createBook, updateBook, deleteBook }
+
--- src/routes/route.mjs
+++ src/routes/route.mjs
@@ -1,6 +1,8 @@
 import express from 'express';
-import {findBook , createBook} from '../controllers/libraryController.mjs';
+import {findBook , createBook, updateBook, deleteBook} from '../controllers/libraryController.mjs';
 const router =express.Router();
 router.get('/books',findBook);
 router.post('/addbooks',createBook);
+router .delete('/deleteBook/:id',deleteBook)
+router.post('/updateBook/:id',updateBook)
 export default router;
